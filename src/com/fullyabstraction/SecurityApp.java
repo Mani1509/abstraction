7831dbed41743f0255b3fd898b210f6512080b9c
Mani1509 <manimaran71298@gmail.com>
2022-09-03 11:36:46 +0530

    mes

---
+++ src/com/fullyabstraction/SecurityApp.java
@@ -0,0 +1,13 @@
+package com.fullyabstraction;
+//fully abstraction
+public interface SecurityApp {
+	public abstract void securityId(int id);
+	
+  public abstract void securityPassword(String password);
+	
+	 
+		
+}
+	
+	
+
---
+++ src/com/fullyabstraction/SecurityOfficial.java
@@ -0,0 +1,31 @@
+package com.fullyabstraction;
+
+public  class SecurityOfficial implements SecurityApp{
+
+	@Override
+	public void securityId(int id) {
+		System.out.println("security id is:"+id);
+		
+	}
+
+	@Override
+	public void securityPassword(String password) {
+	System.out.println("security password is:"+password);
+	
+		
+	}
+	public static void main(String[] args) {
+		SecurityOfficial securityofficial =new SecurityOfficial();
+		securityofficial.securityId(1254);
+		securityofficial.securityPassword("Tcds@123");
+		
+	}
+
+
+		
+	}
+	
+		
+	
+
+
